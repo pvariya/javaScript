a9eb0891d01a19f50885c2a197903b7706fffa30
pvariya <variyapurv4211@gmail.com>
2024-05-20 12:41:04 +0530

    test 2

--- exam/exam 2/index.js
+++ exam/exam 2/index.js
@@ -1,30 +1,48 @@
 let productArray = []
+
 const uimaker = () => {
     document.getElementById("box").innerHTML = ""
-    productArray.map((ele,i) => {
+    productArray.map((ele, i) => {
         let img = document.createElement('img');
         img.src = ele.img
         img.setAttribute("class", "img")
 
         let price = document.createElement('h3');
         price.innerHTML = ele.price
+        price.setAttribute("class", "price")
 
         let productName = document.createElement('h3');
         productName.innerHTML = ele.productName
+        productName.setAttribute("class", "price")
+
+        let delet = document.createElement('button');
+        delet.innerHTML = "DELETE";
+        delet.addEventListener("click", deletProduct = (i) => {
+            productArray.splice(i, 1);
+            uimaker()
+        })
+
+        let buy = document.createElement('button');
+        buy.innerHTML = "BUY";
+        buy.addEventListener("click", () => {
+            alert("Thank you for buying")
+        })
 
         let divImg = document.createElement('div')
-        divImg.append(img, productName, price)
+        divImg.append(img, productName, price, delet, buy)
         divImg.setAttribute("class", "BoxDiv")
-        document.getElementById("box").appendChild(divImg)
+
+        document.getElementById("box").append(divImg)
     })
 }
-
+ 
 const product = (e) => {
     e.preventDefault();
     let data = {
         img: document.getElementById("image").value,
         price: document.getElementById("price").value,
         productName: document.getElementById("product-name").value
+
     }
 
     productArray.push(data)
@@ -33,4 +51,7 @@ const product = (e) => {
     console.log(data);
 }
 
+
+
 document.getElementById("form").addEventListener("submit", product)
+
