1c8337aad6f60ca1c8fa11e7931a469a0c8ed6db
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-07-22 13:57:29 +0530

    Add files via upload

---
+++ finalExam/js/login.js
@@ -0,0 +1,16 @@
+
+import navbar from "../api/userApi.js";
+import { login } from "../component/navbar.js";
+
+document.getElementById("navbar").innerHTML = navbar();
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let user =
+    {
+        username: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+    };
+
+login(user);
+
+});
