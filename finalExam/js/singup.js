9f0f08f2426a75f34f5a061599f1fe7589fec754
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-07-22 14:27:45 +0530

    Update singup.js

--- finalExam/js/singup.js
+++ finalExam/js/singup.js
@@ -1,46 +1,27 @@
 import navbar from "../component/navbar.js"
 document.getElementById("navbar").innerHTML = navbar()
-
+import{createUser,isExists}from"../api/userApi.js"
 
 
 
 
 const signupForm = document.getElementById('signupForm');
-const signupMessage = document.getElementById('signupMessage');
-let id = -1
+
 signupForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const userData = {
+    const user = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
         phone: document.getElementById('phone').value,
         city: document.getElementById('city').value
     };
-    if (id == -1) {
-
-        fetch('https://jsonserver-xpeq.onrender.com/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        })
-            .then(response => response.json())
-            .then(data => {
-                signupMessage.textContent = 'User signed up successfully!';
-                console.log('Success:', data);
-                window.location.href = "/finalExam/index.html"
-
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                signupMessage.textContent = 'Error signing up. Please try again.';
-            });
-    } else {
-        alert("user already exists")
-    }
-
+    
+    localStorage.setItem("isLogin",true);
+    localStorage.setItem("user",JSON.stringify(user));
+    console.log(user)
+    createUser(user);
+    isExists(user);
 });
 
