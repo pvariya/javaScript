4bd1f33678ac7ad1210f72aafd0b955497ce4e21
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-07-27 09:27:54 +0530

    Update logIn.js

--- finalExam2/js/logIn.js
+++ finalExam2/js/logIn.js
@@ -1,14 +1,24 @@
-import navbar from "../navbar/navbar.js";
-document.getElementById("navbar").innerHTML = navbar()
+import navbar  from"../api/navbar.js"
+document.getElementById("navbar").innerHTML=navbar()
 
-import { login } from "../api/userApi.js"
-document.getElementById("navbar").innerHTML = navbar();
-document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    let user =
-    {
-        username: document.getElementById('email').value,
-        password: document.getElementById('password').value,
-    };
-    login(user);
+
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch(
+    `http://localhost:3000/users?username=${username}&password=${password}`
+  );
+  const users = await response.json();
+
+  if (users.length > 0) {
+    localStorage.setItem("loggedInUser", JSON.stringify(users[0]));
+    window.location.href = "/FinalExam2/index.html";
+  } else {
+    alert("User not found. Please sign up.");
+    window.location.href = "/FinalExam2/html/singUp.html";
+  }
 });
