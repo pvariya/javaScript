07da86764b3af015c3f3f375e9ae3fac29c1f59f
variyapurv <133880947+pvariya@users.noreply.github.com>
2024-07-27 09:27:18 +0530

    Update singUp.js

--- finalExam2/js/singUp.js
+++ finalExam2/js/singUp.js
@@ -1,24 +1,40 @@
-import navbar from "../navbar/navbar.js";
-document.getElementById("navbar").innerHTML = navbar()
-import{createUser,isExists}from"../api/userApi.js"
+import navbar  from"../api/navbar.js"
+document.getElementById("navbar").innerHTML=navbar()
+
+document.getElementById("signupForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let newUsername = document.getElementById("newUsername").value
+  let newPassword = document.getElementById("newPassword").value
+  let role=document.getElementById("role").value
 
 
+  const checkResponse = await fetch(
+    `http://localhost:3000/users?username=${newUsername}`
+  );
+  const existingUsersText = await checkResponse.text();
+  const existingUsers = JSON.parse(existingUsersText);
 
-document.getElementById('signUp').addEventListener('submit', (event) => {
-    event.preventDefault();
+  if (existingUsers.length > 0) {
+    alert("User already exists. Please log in.");
+    window.location.href = "/FinalExam2/html/login.html";
+    return;
+  }
 
-    const user = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        Admine_User:document.getElementById('Admine_User').value,
-        password: document.getElementById('password').value,
-    
-    };
-    localStorage.setItem("user",JSON.stringify(user));
-    // console.log(user);
-    localStorage.setItem("isLogin",true);
-    
-    createUser(user);
-    isExists(user);
+  const response = await fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({username: newUsername, password: newPassword , role: role}),
+  });
+
+  const responseText = await response.text();
+
+  if (responseText.includes("id")) {
+    alert("Signup successful! You can now log in.");
+    window.location.href = "/FinalExam2/html/login.html";
+  } else {
+    alert("Signup failed. Please try again.");
+  }
 });
-
